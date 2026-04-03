2765cd1c8ece26f385c497d93669c0096ead5396
Matthew Gertz <mattgertz@hotmail.com>
2026-04-03 15:14:13 -0700

    First copy pass

---
+++ MAUIEuchre/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MAUIEuchre;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MAUIEuchre/Platforms/Android/MainActivity.cs
@@ -0,0 +1,10 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace MAUIEuchre;
+
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ScreenOrientation = ScreenOrientation.Landscape, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
+{
+}
